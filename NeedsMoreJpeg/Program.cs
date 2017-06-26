0168fe74705e3b3e5a0b0f6a1d754597c698f7b5
mrousavy <marcrousavy@hotmail.com>
2017-06-26 18:20:21 +0200

    Added .NET Core solution

---
+++ NeedsMoreJpeg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeedsMoreJpeg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
